870a1d44c366cbc3fbb954cc9c32ad033a9f1392
AntoineAurand <antoineaurand@MacBook-Air-de-Antoine-3.local>
2026-03-01 19:20:28 +0100

    feat: déploiement de la structure complète du projet

---
+++ 01-Foundation/01-deploy-network.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# =====================================================================
+# SCRIPT: 01-deploy-network.sh
+# DESCRIPTION: Deploys the core Azure networking infrastructure.
+# ARCHITECTURE: Hub-style VNet with Management & Identity segmentation.
+# STRATEGY: Zero Trust - All inbound traffic is blocked except for Admin IP.
+# =====================================================================
+
+# --- Variables ---
+RG="RG-LAB-HYBRID-INFRA"
+LOC="belgiumcentral"
+VNET_NAME="VNET-CORE"
+MY_IP=$(curl -s https://ifconfig.me) # https://ifconfig.me : to retrieve my public IP
+
+
+trap 'echo "Deployment failed. Check Azure connection."; exit 1' ERR
+
+
+
+echo "-------------------------------------------------------"
+echo "Starting Azure Foundation Deployment..."
+echo "-------------------------------------------------------"
+
+
+
+# 1. Resource Group creation
+# Why: Logical container for lifecycle management and RBAC.
+az group create --name $RG --location $LOC
+
+
+
+
+# 2. Network Security Group (NSG) setup
+# Why: Acts as a Layer 4 firewall. Created first to ensure security from the start.
+az network nsg create --resource-group $RG --name "NSG-MGMT-ADMIN"
+
+
+
+
+# 3. Security Rule: Restrict RDP access
+# Why: Ports like 3389 (RDP) are highly targeted  
+# How: Add only my current public IP ($MY_IP).
+az network nsg rule create \
+  --resource-group $RG \
+  --nsg-name "NSG-MGMT-ADMIN" \
+  --name "Allow-RDP-From-Home" \
+  --priority 100 \
+  --source-address-prefixes $MY_IP \
+  --destination-port-ranges 3389 \
+  --access Allow \
+  --protocol Tcp \
+  --description "Secure admin access"
+
+
+
+# 4. VNet & Management Subnet (Ultra-Efficient)
+# Why: Using /23 (512 IPs) to perfectly match SME needs and minimize IP waste.
+az network vnet create \
+  --name $VNET_NAME \
+  --resource-group $RG \
+  --location "belgiumcentral" \
+  --address-prefix 10.0.0.0/23 \
+  --subnet-name "Subnet-Management" \
+  --subnet-prefix 10.0.0.0/27 \
+  --network-security-group "NSG-MGMT-ADMIN"
+
+
+
+# 5. Identity Subnet (Domain Controllers)
+az network vnet subnet create \
+  --name "Subnet-Identity" \
+  --address-prefix 10.0.0.32/27 \
+  --resource-group $RG \
+  --vnet-name $VNET_NAME
+
+# 6. Data Subnet (File Services for HR, Finance, IT, Sales)
+az network vnet subnet create \
+  --name "Subnet-Data" \
+  --address-prefix 10.0.1.0/24 \
+  --resource-group $RG \
+  --vnet-name $VNET_NAME 
+
+
+echo "-------------------------------------------------------"
+echo "SUCCESS: Network Infrastructure is live."
+echo "-------------------------------------------------------"
---
+++ 01-Foundation/README.md
@@ -0,0 +1,50 @@
+
+README GENERAL 
+
+
+Azure Secure Hybrid Infrastructure: PME 150 Edition
+
+Vision & Contexte Métier
+Ce projet documente le déploiement automatisé d'une infrastructure IT souveraine pour une PME de 50 à 150 employés. L'enjeu est de fournir un environnement de travail sécurisé, hautement disponible et conforme aux exigences de Souveraineté des données.
+
+
+Les Piliers du Projet
+* Souveraineté (Belgique Central) : Choix de la région belgiumcentral pour garantir une latence minimale et le respect du RGPD en gardant les données sur le sol national.
+* Zero-GUI Policy : Déploiement 100% via Azure CLI et PowerShell pour une IaC, garantissant une reproductibilité parfaite sans erreur humaine.
+* Hardening : Réduction drastique de la surface d'attaque via l'usage de Windows Server 2025 Core et une isolation stricte des flux administratifs.
+
+Architecture Réseau & Segmentation (VNet /23)
+J'ai opté pour une approche "Zero Waste" (512 IP), équilibre idéal entre économie de ressources et capacité d'évolution.
+
+
+
+Ressource	Nom	Adresse/Région	Rôle
+Region	belgiumcentral	Bruxelles	Souveraineté & Latence
+VNet	VNET-CORE	10.0.0.0/23	Enveloppe PME (512 IP)
+Subnet Mgmt	Subnet-Management	10.0.0.0/27	Administration - accès unique via RDP lié dynamiquement à mon adresse IP
+Subnet Id	Subnet-Identity	10.0.0.32/27	Coeur Infra : 2 DC (Haute disponibilité), AD DS, DNS intégré & Entra Connect
+Subnet Data	Subnet-Data	10.0.1.0/24	Zone Métier : Segment pour les 150 collaborateurs (RH, Finance, IT, Sales).
+
+
+
+
+ Focus Stratégique :
+1. Identity (Le Cerveau) : Le choix du /27 permet d'accueillir deux contrôleurs de domaine pour la haute disponibilité et les mises à jour sans interruption de service.
+2. Data (L'Efficience) : Un bloc /24 entier pour une lisibilité parfaite (10.0.1.x). L'isolation entre les services (RH, Finance...) est gérée logiquement pour protéger le trafic de données.
+
+
+
+
+🛠️ Roadmap de Déploiement (Sprint 15 Jours - (cqfd les crédits azure étudiant)
+
+📂 Phase 1 : Network Foundation (✅)
+* Provisionnement du VNet et des subnets segmentés.
+* Sécurisation  via NSG (Network Security Groups).
+📂 Phase 2 : Identity & Hybrid Management ( 🔄)
+* Déploiement du premier contrôleur de domaine sur Windows Server 2025 Core.
+* Configuration de l'AD DS, du DNS interne et préparation du lien Microsoft Entra ID.
+📂 Phase 3 : System Hardening (⏳)
+* Application de GPO de sécurité et durcissement des OS.
+* Mise en place du RBAC (Role-Based Access Control).
+
+
