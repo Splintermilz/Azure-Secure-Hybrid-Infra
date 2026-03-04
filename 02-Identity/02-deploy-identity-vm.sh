3054f2b43bd06e9e80f44ed894cd8d4d5f8680cf
AntoineAurand <antoineaurand@MacBook-Air-de-Antoine-3.local>
2026-03-04 12:10:23 +0100

    feat : Ajout 02-Identity

---
+++ 02-Identity/02-deploy-identity-vm.sh
@@ -0,0 +1,81 @@
+# =====================================================================
+# SCRIPT: 02-deploy-identity-vm.sh
+# DESCRIPTION: Provisioning the first domain controller (DC) via Azure CLI
+# ARCHITECTURE: Windows Server 2025 Core in Subnet-Identity (10.0.0.32/27)
+# STRATEGY: Zero-Trust & Hardening - No public IP, reduced attack surface (Core)
+# =====================================================================
+#
+# 1. Set up .env to dynamically retrieve the VM_ADMIN, VM_PASS and AMDIN_MAIL variables
+if [ -f .env ]; then
+    	source .env
+else
+    echo "Error : Missing .env"
+    exit 1
+fi
+
+# ---  Check if required environment variables are set ---
+required_vars=("VM_ADMIN" "VM_PASS" "ADMIN_MAIL")
+for var in "${required_vars[@]}"; do
+    if [ -z ${!var} ]; then
+        echo "Error: $var is not set in .env file"
+        exit 1
+    fi
+done
+
+
+# --- Variables ---
+RG="RG-LAB-HYBRID-INFRA"
+LOC="belgiumcentral"
+VM_NAME="SRV-AD-01"
+VNET="VNET-CORE"
+SUBNET="Subnet-Identity"
+
+trap 'echo "Deployment failed. Check Azure connection."; exit 1' ERR
+
+echo "Deployment of the VM identity : $VM_NAME ..."
+
+# 2. VM creation  (Hardened)
+# Why : Absence of a public IP address  for isolation from the internet and prevents scans
+# Image : Datacenter-Azure-Edition-Core for Hotpatching
+az vm create \
+  --resource-group $RG \
+  --name $VM_NAME \
+  --image "MicrosoftWindowsServer:WindowsServer:2025-Datacenter-Azure-Edition-Core:latest" \
+  --vnet-name $VNET \
+  --subnet $SUBNET \
+  --admin-username $VM_ADMIN \
+  --admin-password $VM_PASS \
+  --size Standard_B2s \
+  --public-ip-address "" \
+  --storage-sku StandardSSD_LRS
+
+
+
+if [ $? -eq 0 ]; then
+    echo "-------------------------------------------------------"
+    echo "SUCCESS: VM $VM_NAME is created !"
+    echo "-------------------------------------------------------"
+else
+    echo "Error: VM creation failed. Stopping script."
+    exit 1
+fi
+
+
+# 3. FinOps : Auto-shutdown at 7pm
+# Why : Azure credit economy / FinOps mindset
+echo "Configuration of auto-shutdown at 7pm (Brussels Time)..."
+
+az vm auto-shutdown \
+  --resource-group $RG \
+  --name $VM_NAME \
+  --time 1900 \
+  --location $LOC \
+  --email $ADMIN_MAIL # To receive an auto-shutdown confirmation email
+
+if [ $? -eq 0 ]; then
+    echo "-------------------------------------------------------"
+    echo "SUCCESS: Auto-shutdown is enabled."
+    echo "-------------------------------------------------------"
+else
+    echo "Error: VM created, but Auto-shutdown configuration failed."
+fi
